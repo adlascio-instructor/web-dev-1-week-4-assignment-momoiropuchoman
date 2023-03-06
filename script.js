f01b0b03736af9654ef914147a29f1a4a2e4de31
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-03-05 20:56:16 -0800

    Change password visible when the eye icon is clicked

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const passwordTextbox = document.querySelector(".password-textbox")
 passwordTextbox.addEventListener("focus", addFocusToPassword)
 passwordTextbox.addEventListener("blur", removeFocusFromPassword)
 
+const passwordVisibleImage = document.querySelector(".password-visible-image")
+passwordVisibleImage.addEventListener("click", switchPasswordVisibility)
+
 function addFocusToMail() {
     const mailImage = document.querySelector(".mail-image")
     mailImage.src = "images/mail-focus.svg"
@@ -36,4 +39,12 @@ function removeFocusFromPassword() {
 
     const passwordField = document.querySelector(".password-field")
     passwordField.style.border = "1px solid var(--text-base)"
+}
+
+function switchPasswordVisibility() {
+    if(passwordTextbox.type === "text") {
+        passwordTextbox.type = "password"
+    } else {
+        passwordTextbox.type = "text"
+    }
 }
