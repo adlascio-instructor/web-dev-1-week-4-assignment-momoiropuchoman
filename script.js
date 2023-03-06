07fd62a7b6c05e929d2913a03fd76607a2ecf3e3
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-03-05 16:36:34 -0800

    Highlight the input fields when they are focused

---
+++ script.js
@@ -0,0 +1,39 @@
+const mailTextbox = document.querySelector(".mail-textbox")
+mailTextbox.addEventListener("focus", addFocusToMail)
+mailTextbox.addEventListener("blur", removeFocusFromMail)
+
+const passwordTextbox = document.querySelector(".password-textbox")
+passwordTextbox.addEventListener("focus", addFocusToPassword)
+passwordTextbox.addEventListener("blur", removeFocusFromPassword)
+
+function addFocusToMail() {
+    const mailImage = document.querySelector(".mail-image")
+    mailImage.src = "images/mail-focus.svg"
+
+    const mailField = document.querySelector(".mail-field")
+    mailField.style.border = "1px solid var(--primary-color)"
+}
+
+function removeFocusFromMail() {
+    const mailImage = document.querySelector(".mail-image")
+    mailImage.src = "images/mail.svg"
+
+    const mailField = document.querySelector(".mail-field")
+    mailField.style.border = "1px solid var(--text-base)"
+}
+
+function addFocusToPassword() {
+    const lockImage = document.querySelector(".lock-image")
+    lockImage.src = "images/lock-focus.svg"
+
+    const passwordField = document.querySelector(".password-field")
+    passwordField.style.border = "1px solid var(--primary-color)"
+}
+
+function removeFocusFromPassword() {
+    const lockImage = document.querySelector(".lock-image")
+    lockImage.src = "images/mail.svg"
+
+    const passwordField = document.querySelector(".password-field")
+    passwordField.style.border = "1px solid var(--text-base)"
+}
